fe321d7bca52874fcbcd7f9d393f2e944e46c220
AdriaConde <adria.conde@campus.xaloc.org>
2024-09-23 23:03:00 +0200

    Update EJ12_ACT2.py

--- Ejercicios_iniciales/EJ12_ACT2.py
+++ Ejercicios_iniciales/EJ12_ACT2.py
@@ -2,7 +2,7 @@
 var_base_menor=float(input("La base menor es:"))
 var_base_mayor=float(input("La base mayor es:"))
 var_altura=float(input("La altura es:"))
-var_lado=float(input("El es:"))
+var_lado=float(input("El lado es:"))
 var_perimetro=var_base_menor+var_base_mayor+(2*var_lado)
 print("El perímetro es:", var_perimetro)
 var_area=((var_base_menor+var_base_mayor)*var_altura)/2
