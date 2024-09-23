bcdad868a427778ece27b266d53604c7f4954955
AdriaConde <adria.conde@campus.xaloc.org>
2024-09-23 22:45:26 +0200

    Add files via upload

---
+++ Ejercicios_iniciales/EJ13_ACT2.py
@@ -0,0 +1,4 @@
+#13. Realiza un programa que, a partir introducir el lado de un cubo, presente por pantalla el área y para calcular el volumen utiliza el operador de exponente.
+var1=float(input("Introduce el valor de un lado:"))
+print("El área del cubo es:", (var1**2)*6)
+print("El volumen del cubo es:", var1**3)
