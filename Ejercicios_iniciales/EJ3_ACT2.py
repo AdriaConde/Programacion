daa93870159de2dede3a512992d5b2a86e4a2c80
AdriaConde <adria.conde@campus.xaloc.org>
2024-10-18 10:14:55 +0200

    Update EJ3_ACT2.py

--- Ejercicios_iniciales/EJ3_ACT2.py
+++ Ejercicios_iniciales/EJ3_ACT2.py
@@ -1,4 +1,4 @@
 #3. Programa que pida dos números enteros y realice la suma correspondiente
-var1=int(input("Introduce un número entero:"))
+var1=int(input("Introduce un número enteroOOOO:"))
 var2=int(input("Introduce un segundo número entero:"))
 print(f"la suma de {var1} + {var2} es:", var1+var2)
